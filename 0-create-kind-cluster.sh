3bf52df8ac7014b5fdc8fd55f4f8bbfa44795ba3
Carlos Nunez <ncarlos@vmware.com>
2023-01-11 11:54:36 -0600

    Add script to deploy clusters and configurations for each.
    Signed-off-by: Carlos Nunez <ncarlos@vmware.com>

---
+++ 0-create-kind-cluster.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for config_file in "$(dirname "$0")"/conf/clusters/*.yaml
+do
+  cluster_name=tap-$(basename "$config_file" | sed 's/\..*$//')-cluster
+  >&2 echo "===> Installing cluster $cluster_name"
+  kind create cluster --name "$cluster_name" --config "$config_file" || exit 1
+done
