81c7e02cabc354e3f5a629c64cec9b6388edb69a
Carlos Nunez <ncarlos@vmware.com>
2023-01-31 10:46:12 -0600

    begin trying to install TAP
    Signed-off-by: Carlos Nunez <ncarlos@vmware.com>

---
+++ 6-install-tap.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+CERT_PATH="$(dirname "$(realpath "$0")")/.data/tanzu/registry/certs"
+TEMPLATE_PATH="$(dirname "$(realpath "$0")")/conf/profile.tmpl"
+RENDERED_TEMPLATE_PATH="$(dirname "$(realpath "$0")")/.data/tanzu/profiles"
+
+kubernetes_clusters_started() {
+  clusters=$(kind get clusters)
+  for cluster in "$(dirname "$(realpath "$0")")"/conf/clusters/*.yaml
+  do
+    name=tap-$(awk -F '/' '{print $NF}' <<< "$cluster" | cut -f1 -d '.')-cluster
+    grep -q "$name" <<< "$clusters" || return 1
+  done
+}
+
+kubernetes_clusters() {
+  find "$(dirname "$(realpath "$0")")"/conf/clusters/*.yaml -exec basename {} \; | sed 's/.yaml$//'
+}
+
+kubectl_cmd() {
+  cmd=(kubectl --context="kind-tap-$1-cluster" "${@:2}")
+  >&2 echo "========> ${cmd[*]}"
+  "${cmd[@]}"
+}
+
+tanzu_cmd() {
+  kubectl config use-context "kind-tap-$1-cluster"
+  cmd=(/usr/local/bin/tanzu "${@:2}")
+  >&2 echo "[$1] ========> ${cmd[*]}"
+  "${cmd[@]}"
+}
+
+render_profile_template() {
+  test -d "$RENDERED_TEMPLATE_PATH" || mkdir -p "$RENDERED_TEMPLATE_PATH"
+  for cluster in $(kubernetes_clusters)
+  do
+    sed "s/%REGISTRY_CERT_HERE%/$(cat "$CERT_PATH")/" "$TEMPLATE_PATH" |
+      sed "s/%PROFILE_NAME%/$cluster/g" > "${RENDERED_TEMPLATE_PATH}/$cluster.yaml"
+  done
+}
+
+install_tap() {
+  for cluster in $(kubernetes_clusters)
+  do
+    tanzu_cmd "$cluster" package install tap \
+      -p tap.tanzu.vmware.com \
+      -v 1.4.0 \
+      --values-file "${RENDERED_TEMPLATE_PATH}/${cluster}.yaml" \
+      -n tap-install
+  done
+}
+
+create_tap_install_namespace() {
+  for cluster in $(kubernetes_clusters)
+  do
+    kubectl_cmd "$cluster" get ns tap-install ||
+      kubectl_cmd "$cluster" create ns tap-install
+  done
+}
+
+confirm_registry_running() {
+  if ! nc -z localhost 50000
+  then
+    >&2 echo "ERROR: The local Docker registry is probably not running. Run \
+./2-provision-registry.sh to start it up!"
+    return 1
+  fi
+}
+
+confirm_registry_running || exit 1
+
+
+if ! kubernetes_clusters_started
+then >&2 echo "ERROR: None or some Kubernetes clusters missing. \
+Please run 0-create-kind-cluster before running this script."
+  exit 1
+fi
+
+create_tap_install_namespace &&
+  render_profile_template && install_tap
